ac9428b6ba67fe33f1f5caa8d10f3c3c060472c9
Nicky Hoff <nick@hofff.com>
2015-04-28 20:07:24 +0200

    add description and english translation

--- contao-module/languages/de/tl_content.php
+++ contao-module/languages/de/tl_content.php
@@ -4,7 +4,7 @@ $GLOBALS['TL_LANG']['tl_content']['hofff_shariff_legend']
 	= 'Einstellungen für Shariff';
 
 $GLOBALS['TL_LANG']['tl_content']['hofff_shariff_services']
-	= array('Services', '');
+	= array('Services', 'Bitte wählen Sie einen oder mehrere Social Media Services aus, die auf der Website angezeigt werden sollen.');
 $GLOBALS['TL_LANG']['tl_content']['hofff_shariff_services_options'] = array(
 	'facebook' => 'Facebook',
 	'twitter' => 'Twitter',
@@ -14,24 +14,24 @@ $GLOBALS['TL_LANG']['tl_content']['hofff_shariff_services_options'] = array(
 	'info' => 'Info',
 );
 $GLOBALS['TL_LANG']['tl_content']['hofff_shariff_url']
-	= array('URL', '');
+	= array('URL', 'Tragen Sie eine feste URL ein, welche geteilt werden soll. Lassen Sie dieses Feld leer, wenn die aktuelle Seite geteilt werden soll.');
 $GLOBALS['TL_LANG']['tl_content']['hofff_shariff_title']
-	= array('Share-Text', '');
+	= array('Share-Text', 'Tragen Sie einen festen Share-Text ein, der beim Teilen benutzt werden soll. Lassen Sie dieses Feld leer, wenn der Seitentitel der aktuellen Seite benutzt werden soll.');
 $GLOBALS['TL_LANG']['tl_content']['hofff_shariff_referrer_track']
-	= array('Referrer Track', '');
+	= array('Referrer Track', 'Der eingegebene String (z.B. für Tracking-Parameter) wird an die zu teilende URL angehangen.');
 $GLOBALS['TL_LANG']['tl_content']['hofff_shariff_twitter_via']
-	= array('Twitter via', '');
+	= array('Twitter via', 'Bitte tragen Sie einen "Twitter via" Text ein, welcher am Ende des Tweets angefügt wird.');
 $GLOBALS['TL_LANG']['tl_content']['hofff_shariff_orientation']
-	= array('Ausrichtung', '');
+	= array('Ausrichtung', 'Bitte wählen Sie die Ausrichtung der Buttons aus. Zur Auswahl steht horizontal (Buttons nebeneinander) oder vertikal (Buttons untereinander).');
 $GLOBALS['TL_LANG']['tl_content']['hofff_shariff_orientation_options'] = array(
 	'horizontal' => 'Horizontal',
 	'vertical' => 'Vertikal',
 );
 $GLOBALS['TL_LANG']['tl_content']['hofff_shariff_theme']
-	= array('Gestaltung', '');
+	= array('Farbschema', 'Bitte wählen Sie ein Farbschema für die Buttons aus. Zur Auswahl steht das Standardschema (Buttons in den jeweiligen Farben der Services) oder Grau (Buttons einfarbig grau, nur bei Mouse over farbig).');
 $GLOBALS['TL_LANG']['tl_content']['hofff_shariff_theme_options'] = array(
 	'standard' => 'Standard',
 	'grey' => 'Grau',
 );
 $GLOBALS['TL_LANG']['tl_content']['hofff_shariff_share_count']
-	= array('Anzahl der Shares anzeigen', '');
+	= array('Anzahl der Shares anzeigen', 'Bitte aktivieren Sie diese Funktion, wenn die Anzahl der Shares pro Service angezeigt werden soll.');
---
+++ contao-module/languages/en/tl_content.php
@@ -0,0 +1,37 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_content']['hofff_shariff_legend']
+	= 'Shariff settings';
+
+$GLOBALS['TL_LANG']['tl_content']['hofff_shariff_services']
+	= array('Services', 'Please choose which social media service or services you would like to display on your website.');
+$GLOBALS['TL_LANG']['tl_content']['hofff_shariff_services_options'] = array(
+	'facebook' => 'Facebook',
+	'twitter' => 'Twitter',
+	'googleplus' => 'Google+',
+	'whatsapp' => 'WhatsApp',
+	'mail' => 'Mail',
+	'info' => 'Info',
+);
+$GLOBALS['TL_LANG']['tl_content']['hofff_shariff_url']
+	= array('URL', 'Enter a fixed URL to be shared. Leave this field blank if you want to share the current page.');
+$GLOBALS['TL_LANG']['tl_content']['hofff_shariff_title']
+	= array('Share-Text', 'Enter a fixed text to be used for sharing. Leave this field blank if you want to use the page title of the current page.');
+$GLOBALS['TL_LANG']['tl_content']['hofff_shariff_referrer_track']
+	= array('Referrer Track', 'The entered string (e.g. tracking parameter) will be appended to the sharing url.');
+$GLOBALS['TL_LANG']['tl_content']['hofff_shariff_twitter_via']
+	= array('Twitter via', 'Please enter a "Twitter via" text which will be added at the end of the tweet.');
+$GLOBALS['TL_LANG']['tl_content']['hofff_shariff_orientation']
+	= array('Alignment', 'Please choose how you would like to align the buttons. The choices are horizontal (buttons side by side) or vertical (buttons one below the other).');
+$GLOBALS['TL_LANG']['tl_content']['hofff_shariff_orientation_options'] = array(
+	'horizontal' => 'horizontal',
+	'vertical' => 'vertical',
+);
+$GLOBALS['TL_LANG']['tl_content']['hofff_shariff_theme']
+	= array('Color Theme', 'Please choose a color theme for the buttons. The choices are standard (buttons will use the color of each service) or gray (buttons monochrome gray, color only on mouse-over).');
+$GLOBALS['TL_LANG']['tl_content']['hofff_shariff_theme_options'] = array(
+	'standard' => 'standard',
+	'grey' => 'gray',
+);
+$GLOBALS['TL_LANG']['tl_content']['hofff_shariff_share_count']
+	= array('Show number of shares', 'Activate this checkbox if you want to display the number of shares in each service.');
