b13a3f2d06df4d4dc2056fcc18d6f8f8f7a49918
Oliver Hoff <oliver@hofff.com>
2015-09-16 13:04:09 +0200

    fix counts.php require

--- contao-module/endpoints/counts.php
+++ contao-module/endpoints/counts.php
@@ -3,6 +3,6 @@
 define('TL_SCRIPT', 'counts.php');
 
 define('TL_MODE', 'FE');
-require __DIR__ . '../../../initialize.php';
+require __DIR__ . '/../../../initialize.php';
 
 (new Hofff\Contao\Shariff\CountsController)->run();
