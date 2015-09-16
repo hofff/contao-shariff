b14a60a7663990082551457bb8122e59927679ec
Oliver Hoff <oliver@hofff.com>
2015-09-16 16:04:50 +0200

    fix problems with counts.php requiring initialize.php when installed via symlinks from composer vendors

--- contao-module/endpoints/counts.php
+++ contao-module/endpoints/counts.php
@@ -1,8 +1,13 @@
 <?php
 
 define('TL_SCRIPT', 'counts.php');
-
 define('TL_MODE', 'FE');
-require __DIR__ . '/../../../initialize.php';
+
+$init = __DIR__ . '/../../../../../../system/initialize.php';
+if(is_file($init)) { // module installed via symlinks from composer vendors
+	require $init;
+} else { // module copied into modules dir
+	require __DIR__ . '/../../../initialize.php';
+}
 
 (new Hofff\Contao\Shariff\CountsController)->run();
