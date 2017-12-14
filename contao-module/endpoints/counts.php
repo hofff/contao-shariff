898e9da4976f3195144c545c88a801d12a8fdc01
Yanick Witschi <yanick.witschi@terminal42.ch>
2017-12-14 13:25:30 +0100

    Support Contao 4

--- contao-module/endpoints/counts.php
+++ contao-module/endpoints/counts.php
@@ -3,11 +3,16 @@
 define('TL_SCRIPT', 'counts.php');
 define('TL_MODE', 'FE');
 
-$init = __DIR__ . '/../../../../../../system/initialize.php';
-if(is_file($init)) { // module installed via symlinks from composer vendors
-	require $init;
-} else { // module copied into modules dir
-	require __DIR__ . '/../../../initialize.php';
+foreach ([
+    __DIR__ . '/../../../initialize.php', // system/modules (Contao 3.5 and Contao 4 if installed manually)
+    __DIR__ . '/../../../../../../../system/initialize.php', // Contao 3.5 with Composer
+    __DIR__ . '/../../../../../../system/initialize.php', // Contao 4 with Composer
+         ] as $script
+) {
+    if (file_exists($script)) {
+        require_once($script);
+        break;
+    }
 }
 
 (new Hofff\Contao\Shariff\CountsController)->run();
