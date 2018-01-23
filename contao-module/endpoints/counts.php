7fecfc1fcf1b7b86ddfa616e224b64c8c04bb80c
backbone <backbone@DESKTOP-P4C0KGJ>
2018-01-23 14:33:46 +0100

    fix contao 4 share counts endpoint

--- contao-module/endpoints/counts.php
+++ contao-module/endpoints/counts.php
@@ -6,7 +6,7 @@ define('TL_MODE', 'FE');
 foreach([
     __DIR__ . '/../../../initialize.php', // system/modules (Contao 3.5 and Contao 4 if installed manually)
     __DIR__ . '/../../../../../../../system/initialize.php', // Contao 3.5 with Composer
-    __DIR__ . '/../../../../../../system/initialize.php', // Contao 4 with Composer
+    __DIR__ . '/../../../../../system/initialize.php', // Contao 4 with Composer
 ] as $script) {
     if(file_exists($script)) {
         require_once $script;
